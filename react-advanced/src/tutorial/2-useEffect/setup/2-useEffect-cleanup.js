64c079a5b338b999f5f32befa3bf18f7e3bf50d9
unknown <pranavkalwad@gmail.com>
2022-07-03 16:51:56 +0530

    section 52 done

--- react-advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-advanced/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("UseEffect");
+    window.addEventListener("resize", checkSize);
+    return () => {
+      console.log("Clean up");
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+  console.log("Render");
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size} Px</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
